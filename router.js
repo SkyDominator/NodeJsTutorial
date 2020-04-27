a233decda87002a050372f2381fa048ef0af4697
ray <ray.kim@groundx.xyz>
2020-04-27 14:50:58 +0900

    sdfsdf

--- router.js
+++ router.js
@@ -1,11 +1,15 @@
-function route(handle, pathname){
-    console.log("About to route a request for" + pathname)
-    if (typeof handle[pathname] === 'function'){
-        handle[pathname]()
+function route(pathname, handle, response){
+    console.log("About to route a request for " + pathname + "\n")
+    if (typeof handle[pathname]=='function'){
+        handle[pathname](response)
     }
     else{
-        console.log("No request handler for: " + pathname)
+        console.log("No request found for " + pathname)
+        response.writeHead(404, {"Content-Type": "text/plain"})
+        response.write("404 Not Found")
+        response.end()
     }
 }
 
 exports.route=route
+
